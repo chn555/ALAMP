7f94d5d1af0036a1ee0307a0cb8b17d8a46f523f
chn555 <chn566@gmail.com>
2018-04-24 00:27:03 +0300

    s

--- script.sh
+++ script.sh
@@ -4,20 +4,20 @@
 # 21/4/18
 
 Install_Httpd () {
-sudo yum install httpd
+sudo yum install httpd -y
 sudo systemctl start httpd.service
 sudo systemctl enable httpd.service
 }
 
 Install_Mariadb () {
-sudo yum install mariadb-server mariadb
+sudo yum install mariadb-server mariadb -y
 sudo systemctl start mariadb
 sudo mysql_secure_installation
 sudo systemctl enable mariadb.service
 }
 
 Install_Php () {
-sudo yum install php php-mysql
+sudo yum install php php-mysql -y
 sudo systemctl restart httpd.service
 sudo printf "<?php phpinfo(); ?>" > /var/www/html/info.php
 }
